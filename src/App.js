4d0e0987ab02434ddad416140b8ae08d86fffda0
Hai Pham <haipham.ndev@gmail.com>
2022-05-04 10:50:10 +0700

    Add feature album

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import AlbumFeature from "./features/Album";
 import TodoFeature from "./features/Todo";
 // import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      <AlbumFeature />
     </div>
   );
 }
