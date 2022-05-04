7a47fbbc8fe9a3ec32404ec13f6a60a6d81644aa
Hai Pham <haipham.ndev@gmail.com>
2022-05-04 10:01:49 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 // import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to ReactJS</div>;
+  return <div className="App">Welcome to ReactJS Advanced</div>;
 }
 
 export default App;
